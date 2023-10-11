42860056f6f525225f34627ca47c758a44338f8e
KEVIN-BRET <kevinbret.pro@gmail.com>
2023-10-11 19:55:50 +0200

    CSS menu mobile (wip)

---
+++ index.js
@@ -0,0 +1,20 @@
+// const burgerBtn = document.querySelector(".burgerBtn");
+
+// const allNavLinks = document.querySelector(".all-nav-links");
+
+// console.log(allNavLinks);
+
+// burgerBtn.addEventListener("click", () => {
+//   allNavLinks.style.display = "flex";
+// });
+
+// burgerBtn.addEventListener("click", () => {
+//   allNavLinks.style.display = "flex";
+// });
+
+// // ou en cliaquant à coté de la modale :
+// window.addEventListener("click", (e) => {
+//   if (e.target !== allNavLinks) {
+//     allNavLinks.style.display = "none";
+//   }
+// });
