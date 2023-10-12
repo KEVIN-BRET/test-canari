c0363707744728f0f52a9f5b462963f482c68552
KEVIN-BRET <kevinbret.pro@gmail.com>
2023-10-12 11:35:28 +0200

    Mobile menu

--- index.js
+++ index.js
@@ -1,20 +1,11 @@
-// const burgerBtn = document.querySelector(".burgerBtn");
+const burgerBtn = document.querySelector(".burgerBtn");
+const allNavLinks = document.querySelector(".all-nav-links");
 
-// const allNavLinks = document.querySelector(".all-nav-links");
+const activeElement = document.querySelectorAll(".active");
 
-// console.log(allNavLinks);
+// Menu mobile :
+burgerBtn.addEventListener("click", () => {
+  allNavLinks.classList.toggle("mobile-menu");
+});
 
-// burgerBtn.addEventListener("click", () => {
-//   allNavLinks.style.display = "flex";
-// });
-
-// burgerBtn.addEventListener("click", () => {
-//   allNavLinks.style.display = "flex";
-// });
-
-// // ou en cliaquant à coté de la modale :
-// window.addEventListener("click", (e) => {
-//   if (e.target !== allNavLinks) {
-//     allNavLinks.style.display = "none";
-//   }
-// });
+// //** NAVIGATION LINKS **//
