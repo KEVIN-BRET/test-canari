6f948e54fac3cc9d4d2f27e66d1276e911178d59
KEVIN-BRET <kevinbret.pro@gmail.com>
2023-10-12 14:14:28 +0200

    Burger button bugged

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const burgerBtn = document.querySelector(".burgerBtn");
+const closeBtn = document.querySelector(".closeBtn");
 const allNavLinks = document.querySelector(".all-nav-links");
 
 // Open/close mobile menu :
 burgerBtn.addEventListener("click", () => {
   allNavLinks.classList.toggle("mobile-menu");
+  burgerBtn.style.display = "none";
+  closeBtn.style.display = "flex";
+});
+
+closeBtn.addEventListener("click", () => {
+  allNavLinks.classList.toggle("mobile-menu");
+  closeBtn.style.display = "none";
+  burgerBtn.style.display = "flex";
 });
